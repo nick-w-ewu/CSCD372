55b55695e59e97f9e60e495be453bda201e1c892
nick-w-ewu <nicholas.witmer@eagles.ewu.edu>
2015-10-02 10:33:15 -0700

    starting 2nd activity

--- NWitmerLab1/app/src/main/java/com/witmer/nicholas/nwitmerlab1/MainActivity.java
+++ NWitmerLab1/app/src/main/java/com/witmer/nicholas/nwitmerlab1/MainActivity.java
@@ -7,33 +7,43 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
+        }
+        if(id == R.id.action_about)
+        {
+            Toast.makeText(getApplicationContext(), "Android Lab 1, Nicholas Witmer", Toast.LENGTH_SHORT).show();
         }
 
         return super.onOptionsItemSelected(item);
@@ -41,7 +51,16 @@ public class MainActivity extends AppCompatActivity {
 
     public void surveyClick(View v)
     {
-        Toast.makeText(getApplicationContext(), "Survey Clicked!!!!!!", Toast.LENGTH_LONG).show();
+        EditText editField = (EditText)findViewById(R.id.editText);
+        String name = editField.getText().toString();
+        if(name.equals(""))
+        {
+            Toast.makeText(getApplicationContext(), "Enter a name", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), name, Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void websiteClick(View v)
