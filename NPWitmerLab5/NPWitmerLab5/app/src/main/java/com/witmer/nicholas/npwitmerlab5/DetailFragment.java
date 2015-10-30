f0a49d696b18a7ee8e990046bbbd94198046ce34
nick-w-ewu <nicholas.witmer@eagles.ewu.edu>
2015-10-30 13:50:57 -0700

    Lab 5 Friday

--- NPWitmerLab5/NPWitmerLab5/app/src/main/java/com/witmer/nicholas/npwitmerlab5/DetailFragment.java
+++ NPWitmerLab5/NPWitmerLab5/app/src/main/java/com/witmer/nicholas/npwitmerlab5/DetailFragment.java
@@ -25,7 +25,15 @@ public class DetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        View v = inflater.inflate(R.layout.fragment_detail, container, false);
+        Bundle b = getArguments();
+        if(b != null);
+        {
+            String time = b.getString("time");
+            TextView text = (TextView)v.findViewById(R.id.details);
+            text.setText(time);
+        }
+        return v;
     }
 
     public void updateText(String update)
--- NPWitmerLab5/NPWitmerLab5/app/src/main/java/com/witmer/nicholas/npwitmerlab5/MainActivity.java
+++ NPWitmerLab5/NPWitmerLab5/app/src/main/java/com/witmer/nicholas/npwitmerlab5/MainActivity.java
@@ -1,12 +1,14 @@
 package com.witmer.nicholas.npwitmerlab5;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.telecom.Call;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,7 +23,19 @@ public class MainActivity extends AppCompatActivity implements MainFragment.Upda
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        if (savedInstanceState == null) {
+            getFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.portHolder, new MainFragment())
+                    .commit();
+        }
     }
+
+    private boolean landscapeOrientation()
+    {
+        return getResources().getBoolean(R.bool.dual_pane);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -48,6 +62,31 @@ public class MainActivity extends AppCompatActivity implements MainFragment.Upda
     @Override
     public void onFragmentInteraction(String data)
     {
-        Fragment toUpdate = getSupportFragmentManager().findFragmentById(R.id.detailHolder);
+        DetailFragment toUpdate;
+        if(landscapeOrientation())
+        {
+            toUpdate = (DetailFragment) getFragmentManager().findFragmentById(R.id.detailHolder);
+            toUpdate.updateText(data);
+        }
+        else
+        {
+            toUpdate = (DetailFragment)getFragmentManager().findFragmentByTag("DETAIL_FRAGMENT");
+            if(toUpdate != null)
+            {
+                Log.d("Good", "Details fragment found");
+                toUpdate.updateText(data);
+            }
+            else
+            {
+                Log.d("Bad", "No Details Fragment found, creating new");
+                toUpdate = new DetailFragment();
+                Bundle b = new Bundle();
+                b.putString("time", data);
+                toUpdate.setArguments(b);
+                FragmentTransaction trans = getFragmentManager().beginTransaction();
+                trans.replace(R.id.portHolder, toUpdate, "DETAIL_FRAGMENT");
+                trans.commit();
+            }
+        }
     }
 }
