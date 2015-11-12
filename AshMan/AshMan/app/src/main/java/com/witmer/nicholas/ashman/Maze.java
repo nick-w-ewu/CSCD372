f43ed55dcfd4bba1b19dbfb1f1cce404fb2295e2
nick-w-ewu <nicholas.witmer@eagles.ewu.edu>
2015-11-12 15:15:28 -0800

    Term project started

---
+++ AshMan/AshMan/app/src/main/java/com/witmer/nicholas/ashman/Maze.java
@@ -0,0 +1,27 @@
+package com.witmer.nicholas.ashman;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by nicho on 11/12/2015.
+ */
+public class Maze extends View
+{
+
+    public Maze(Context context)
+    {
+        super(context);
+    }
+
+    public Maze(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+    public Maze(Context context, AttributeSet attrs, int defStyleAttr)
+    {
+        super(context, attrs, defStyleAttr);
+    }
+}
